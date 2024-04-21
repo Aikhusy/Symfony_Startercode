644f1d5b6a2de53aa99e0bf184a27d7d928b24d2
Syaikhul Shafwan R <heymepoo@gmail.com>
2024-04-21 15:16:20 +0700

    add : Index for User controller

---
+++ src/Controller/UserController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/UserController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'user_index')]
+    public function index(): Response
+    {
+        return $this->render('layout/simple.html.twig');
+    }
+}
